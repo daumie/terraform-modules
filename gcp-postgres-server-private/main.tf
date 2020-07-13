cf7d59f056813cbae3f12658a0534d0ce4752058
Dominic Motuka <dominic.motuka@gmail.com>
2020-07-13 15:54:48 +0300

    Set module dependency resource
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- gcp-postgres-server-private/main.tf
+++ gcp-postgres-server-private/main.tf
@@ -92,6 +92,20 @@ resource "google_sql_database_instance" "this" {
   }
 }
 
+# ------------------------------------------------------------------------------
+# SET MODULE DEPENDENCY RESOURCE
+# This works around a terraform limitation where we can not specify module dependencies natively.
+# See https://github.com/hashicorp/terraform/issues/1178 for more discussion.
+# By resolving and computing the dependencies list, we are able to make all the resources in this module depend on the
+# resources backing the values in the dependencies list.
+# ------------------------------------------------------------------------------
+
+resource "null_resource" "dependency_getter" {
+  provisioner "local-exec" {
+    command = "echo ${length(var.dependencies)}"
+  }
+}
+
 
 
 resource "random_password" "default_password" {
