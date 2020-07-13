875b99b2bbfc9ebd0a8d7bd2c5798a8de48687f0
Dominic Motuka <dominic.motuka@gmail.com>
2020-07-13 15:58:21 +0300

    Remove default dependency
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- gcp-postgres-server-private/inputs.tf
+++ gcp-postgres-server-private/inputs.tf
@@ -147,6 +147,6 @@ variable "resource_timeout" {
 variable "dependencies" {
   description = "Create a dependency between the resources in this module to the interpolated values in this list (and thus the source resources). In other words, the resources in this module will now depend on the resources backing the values in this list such that those resources need to be created before the resources in this module, and the resources in this module need to be destroyed before the resources in the list."
   type        = list(string)
-  default     = ["google_service_networking_connection.dev_private_vpc_connection"]
+  default     = []
 }
 
