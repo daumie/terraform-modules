3bb663189d32f1ff74704f79487cd6e7ddb54f97
Dominic Motuka <dominic.motuka@gmail.com>
2020-07-13 22:25:08 +0300

    Specify versions to use
    Signed-off-by: Dominic Motuka <dominic.motuka@gmail.com>

--- gcp-postgres-server-private/versions.tf
+++ gcp-postgres-server-private/versions.tf
@@ -1,4 +1,10 @@
 terraform {
   required_version = ">= 0.12"
-  
+
+  required_providers {
+    google      = ">= 2.11"
+    google-beta = ">= 2.11"
+    random      = ">= 2.2"
+    vault       = ">= 2.1"
+  }
 }
